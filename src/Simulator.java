df376a841ee922f256382f710f0f48b65e5f5aa4
Belal Hamdy <36121094+belalhamdy@users.noreply.github.com>
2019-12-13 11:46:57 +0200

    Added some of Itype and Jtype instructions

--- src/Simulator.java
+++ src/Simulator.java
@@ -6,7 +6,7 @@ public class Simulator {
     private List<Instruction> instructions;
     private int nextInstrcutionIdx; // Acts like program counter but uses index in the list | -1 -> finish
     private final int memoryOffset; // start of the program in the memory
-    static final int MEMORY_SIZE = (1<<15),REGISTER_SIZE = (1<<5);
+    static final int MEMORY_SIZE = (1<<15),REGISTER_SIZE = (1<<5),BYTE = 0xFF,HALF_WORD = 0xFF,MEMORY_START = 0x00001000;
     private int[] memory , registers;
 
     Simulator(List<Instruction> instructions,int memoryOffset , int[] memory, int[] registers){
@@ -57,6 +57,7 @@ public class Simulator {
         // TODO: execute all I type instructions here given an instruction of I type as a parameter and return false if you faced any error
         // TODO: Also update here the program counter
         ITypeInstruction command = (ITypeInstruction) current;
+
         int RSValue = getFromRegisters(command.getRS());
         int RTValue = getFromRegisters(command.getRT());
         int imm = command.getImmediate();
@@ -66,64 +67,88 @@ public class Simulator {
 
         switch (command.getCommand()){
             case lb:
+            case lbu:
+                RTValue = getFromMemory(imm) & BYTE;
                 break;
             case lh:
+            case lhu:
+                RTValue = getFromMemory(imm) & HALF_WORD;
                 break;
             case lw:
+                RTValue = getFromMemory(imm);
                 break;
             case sb:
+                setToMemory(RTValue & BYTE,imm);
                 break;
             case sh:
+                setToMemory(RTValue & HALF_WORD,imm);
                 break;
             case sw:
-                break;
-            case beq:
-                break;
-            case bne:
-                break;
-            case lbu:
-                break;
-            case lhu:
+                setToMemory(RTValue,imm);
                 break;
             case lui:
-                break;
-            case ori:
-                break;
-            case addi:
+                RTValue = imm << 16;
                 break;
             case andi:
+                RTValue = RSValue & imm;
                 break;
-            case slti:
+            case ori:
+                RTValue = RSValue | imm;
                 break;
             case xori:
+                RTValue = RSValue ^ imm;
                 break;
+            case addi:
             case addiu:
+                RTValue = RSValue + imm;
                 break;
             case sltiu:
+            case slti:
+                RTValue = RSValue<imm?1:0;
+                break;
+            case beq:
+                if (RSValue == RTValue) nextInstrcutionIdx = gotoLabel(imm);
+                break;
+            case bne:
+                if (RSValue != RTValue) nextInstrcutionIdx = gotoLabel(imm);
                 break;
             default:
                 return false;
         }
 
-        setToRegister(RTValue,command.getRT());
+        setToRegister(RTValue,command.getRT()); // in case of jump RT will not change so it will set it's value with the same old value
         return true;
     }
     private boolean JInstructionExecute(Instruction current){
         // TODO: execute all J type instructions here given an instruction of J type as a parameter and return false if you faced any error
         // TODO: Also update here the program counter
+        JTypeInstruction command = (JTypeInstruction) current;
+        int add = command.getAddr();
+
+        switch (command.getCommand()){
+            case j:
+                nextInstrcutionIdx = gotoLabel(add);
+                break;
+            case jal:
+                // TODO: see the difference between them
+                nextInstrcutionIdx = gotoLabel(add);
+                break;
+            default:
+                return false;
+        }
         return true;
     }
-    private boolean executeLabel(int label){
-        //@TODO: make it takes label as input and modify in the program counter to the label
-        return true;
+    private int gotoLabel(int label){
+        //@TODO: make it takes label as input and returns label index
+        return 0;
     }
     // Takes index not offset
     private int getFromMemory(int memoryIndex){
-        return memory[memoryIndex];
+        return memory[memoryIndex*4 + MEMORY_START];
     }
     // Takes index not offset
     private boolean setToMemory(int data, int memoryIndex){
-        memory[memoryIndex] = data;
+        memory[memoryIndex*4 + MEMORY_START] = data;
         return true;
     }
     private int getFromRegisters(int registerIndex){
