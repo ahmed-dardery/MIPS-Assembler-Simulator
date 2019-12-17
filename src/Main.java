ca9e47e5842b1fb5608e1db3dda8f8ca36dbad05
Belal Hamdy <36121094+belalhamdy@users.noreply.github.com>
2019-12-17 22:26:28 +0200

    Refactoring

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,10 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
-
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception ignored) {
+        }
         SimulatorForm f = new SimulatorForm(new Simulator());
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setVisible(true);
