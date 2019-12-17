f4ed7030c1e8614e307aead9304bd453d713a26f
adhammamdouh <36306083+adhammamdouh@users.noreply.github.com>
2019-12-17 04:48:38 +0200

    Updated Simulator Form

--- src/SimulatorForm.java
+++ src/SimulatorForm.java
@@ -1,12 +1,16 @@
 import org.jfree.ui.RefineryUtilities;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.filechooser.FileSystemView;
 import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class SimulatorForm extends JFrame {
     private JPanel panel;
@@ -17,7 +21,7 @@ public class SimulatorForm extends JFrame {
     private JButton clearRegistersButton;
     private JButton clearMemoryButton;
     private JButton uploadCodeButton;
-    private JTextPane instructionsPane;
+    private JTextPane codePane;
     private JScrollPane instructionScrollPanel;
     private JScrollPane memoryScrollPanel;
     private JScrollPane registerScroll;
@@ -28,22 +32,48 @@ public class SimulatorForm extends JFrame {
     private JPanel runPanel;
     private JTextPane instructionDecPane;
     private JTextField currebtInstructionTxt;
+    private JTextField textField1;
 
-    private File CodeFile;
+    private static final int MEMORY_SIZE = (1 << 15), REGISTER_SIZE = (1 << 5);
     private String assemblyCode;
     private int[] memory, registers;
 
 
     private void buildMemory() {
         DefaultTableModel memoryModel = (DefaultTableModel) memoryTable.getModel();
+
+        memoryModel.setColumnCount(0);
         memoryModel.addColumn("Address");
         memoryModel.addColumn("Value");
+
+        memoryModel.setRowCount(0);
+        for(int i = 0 ; i < MEMORY_SIZE ; ++i){
+            //TODO edit the address(i) that added to the table row
+            memoryModel.addRow(new Object[]{i, memory[i]});
+        }
     }
 
     private void buildRegister() {
-        DefaultTableModel memoryModel = (DefaultTableModel) registerTable.getModel();
-        memoryModel.addColumn("Address");
-        memoryModel.addColumn("Value");
+        DefaultTableModel registerModel = (DefaultTableModel) registerTable.getModel();
+
+        registerModel.setColumnCount(0);
+        registerModel.addColumn("Address");
+        registerModel.addColumn("Value");
+
+        registerModel.setRowCount(0);
+        for(int i = 0 ; i < REGISTER_SIZE ; ++i){
+            //TODO edit the address(i) that added to the table row
+            registerModel.addRow(new Object[]{i, registers[i]});
+        }
+    }
+
+    private void readAndSetCode(File assemblyCodeFile) throws FileNotFoundException {
+        Scanner s = new Scanner(assemblyCodeFile);
+
+        while (s.hasNextLine())
+            assemblyCode += s.nextLine() + '\n';
+
+        codePane.setText(assemblyCode);
     }
 
     SimulatorForm(int[] memory, int[] registers) {
@@ -61,19 +91,33 @@ public class SimulatorForm extends JFrame {
         clearMemoryButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //TODO Clear Memory
+                clearMemory();
             }
         });
         clearRegistersButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //TODO Clear Register
+                clearRegisters();
             }
         });
         uploadCodeButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //TODO Upload File
+                File assemblyCodeFile;
+                JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+                int returnValue = jfc.showOpenDialog(null);
+                FileNameExtensionFilter filter = new FileNameExtensionFilter("Text files", "txt");
+                jfc.addChoosableFileFilter(filter);
+
+                try {
+                    if (returnValue == JFileChooser.APPROVE_OPTION) {
+                        assemblyCodeFile = jfc.getSelectedFile();
+                        readAndSetCode(assemblyCodeFile);
+                    } else
+                        throw new Exception("Can not upload the Selected File");
+                } catch (Exception e1) {
+                    e1.printStackTrace();
+                }
             }
         });
         runInstructionButton.addActionListener(new ActionListener() {
@@ -109,6 +153,12 @@ public class SimulatorForm extends JFrame {
                 //TODO USE Row, Col to update the registers
             }
         });
+        textField1.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                //TODO Update Program Counter
+            }
+        });
     }
 
     public void addException(Exception e) {
@@ -125,7 +175,25 @@ public class SimulatorForm extends JFrame {
         instructionDecPane.setText(des);
     }
 
+    public void clearMemory(){
+        for(int i = 0 ; i < MEMORY_SIZE ; ++i){
+            //TODO Add Getter and Setter if you want.
+            memory[i] = 0;
+        }
+        buildMemory();
+    }
+
+    public void clearRegisters(){
+        for(int i = 0 ; i < REGISTER_SIZE ; ++i){
+            //TODO Add Getter and Setter if you want.
+            registers[i] = 0;
+        }
+        buildRegister();
+    }
+
     public static void main(String[] args) {
-        SimulatorForm sf = new SimulatorForm(null, null);
+        int[] memory = new int[(1 << 15)];
+        int[] register= new int[(1 << 5)];
+        SimulatorForm sf = new SimulatorForm(memory, register);
     }
 }
